cc764df50298176326393ea5af3beaa582231966
MesfinGithinji <meshgithinji@gmail.com>
2023-11-28 01:05:19 +0300

    blockscoped variables and rule definition for eslint

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // eslint-disable-next-line no-empty
+  }
+
+  return [task, task2];
+}
