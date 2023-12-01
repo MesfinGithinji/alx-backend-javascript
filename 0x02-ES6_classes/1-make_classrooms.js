ce2c0f2aa4ab6e2681e2f48d1bda75c2978c446a
MesfinGithinji <meshgithinji@gmail.com>
2023-12-01 04:04:55 +0300

    Class method

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,6 +4,7 @@ import ClassRoom from './0-classroom';
  * Method that instantiates 3 ClassRoom objects
  * returns an array of the specific sizes
  */
+
 export default function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
 }
