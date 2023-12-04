131ac894a6704cfb9fcf6bbbbd85e95dc8799e0b
MesfinGithinji <meshgithinji@gmail.com>
2023-12-04 16:41:19 +0300

    Data Manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+	const students =[
+		{
+		"id": 1,
+		"firstname": "Guillaume",
+		"location": "San Francisco"
+		},
+		{
+		"id": 2,
+                "firstname": "James",
+                "location": "Coloumbia"
+		},
+		{
+		"id": 5,
+                "firstname": "Serena",
+                "location": "San Francisco"
+		}
+	];
+
+	return students;
+}
