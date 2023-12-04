ff8692537c19c30c05c73fe58baf07292085c0eb
MesfinGithinji <meshgithinji@gmail.com>
2023-12-05 01:27:16 +0300

    Use of the reduce function

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
-import getListStudents from './0-get_list_students';
-
-export default function getStudentIdsSum(students = getListStudents()) {
+export default function getStudentIdsSum(students) {
   return students.reduce((first, next) => first + next.id, 0);
 }
