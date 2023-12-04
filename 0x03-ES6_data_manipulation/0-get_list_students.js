566ffca008175c9024729a7b69c0a81dd86d302c
MesfinGithinji <meshgithinji@gmail.com>
2023-12-04 16:45:05 +0300

    Returning an array of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,21 @@
 export default function getListStudents() {
-	const students =[
-		{
-		"id": 1,
-		"firstname": "Guillaume",
-		"location": "San Francisco"
-		},
-		{
-		"id": 2,
-                "firstname": "James",
-                "location": "Coloumbia"
-		},
-		{
-		"id": 5,
-                "firstname": "Serena",
-                "location": "San Francisco"
-		}
-	];
+  const students = [
+    {
+      id: 1,
+      firstname: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstname: 'James',
+      location: 'Coloumbia',
+    },
+    {
+      id: 5,
+      firstname: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
 
-	return students;
+  return students;
 }
