41631b798520025d1684ae41618a6f6065d09603
MesfinGithinji <meshgithinji@gmail.com>
2023-12-05 21:53:41 +0300

    search through a string within a set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,10 @@
 export default function cleanSet(set, startString) {
   const strings = [];
+
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
+
   for (const value of set.values()) {
     if (typeof value === 'string' && value.startsWith(startString)) {
       const valueSubStr = value.substring(startString.length);
